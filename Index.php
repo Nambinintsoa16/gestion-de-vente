959848d6d4def2b298778c593e6c50ff4ffd54ce
Nambinintsoa16 <43542471+Nambinintsoa16@users.noreply.github.com>
2019-02-21 08:05:07 +0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="img/icons/logo.ico">
+  <title>Gestion de vente sur Facebook</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-theme.css" rel="stylesheet">
+  <link href="css/elegant-icons-style.css" rel="stylesheet" />
+  <link href="css/font-awesome.css" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/style-responsive.css" rel="stylesheet" />
+</head>
+<body class="login-img3-body">
+
+  <div class="container">
+    <form class="login-form">
+      <div class="login-wrap">
+        <p class="login-img"><i class="icon_lock_alt"></i></p>
+        <div class="input-group">
+          <span class="input-group-addon"><i class="icon_profile"></i></span>
+          <input type="text" class="form-control" name="user" placeholder="Nom d'utilisateur" autofocus id="user">
+        </div>
+        <div class="input-group">
+          <span class="input-group-addon"><i class="icon_key_alt"></i></span>
+          <input type="password" class="form-control" name="pass" placeholder="Mot de passe" id="pass">
+        </div>
+        <button class="btn btn-primary btn-lg btn-block" type="submit">Se connecter</button>
+     
+      </div>
+      <div class="credits" >
+        <p style="color:red;" class="error"> </p>
+      </div>
+    </form>
+    
+  </div>
+
+
+</body>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/fonctionLogin.js"></script>
+</html>
